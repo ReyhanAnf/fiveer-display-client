018be376d05ba4cdf033fb080084bae938d2e3c4
Reyhan Anderea Firdaus <reihan676590@gmail.com>
2023-12-17 19:58:14 +0700

    additional commits

---
+++ coba/app.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+from PIL import Image
+
+st.set_page_config(page_title='Survey Results')
+st.header('Survey Results 20221')
+st.subheader('was the tutorial helpful?')
