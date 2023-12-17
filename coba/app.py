f791d2e940a2bf6545eab29fbf2b8eb2650fa1d7
Reyhan Andrea Firdaus <77052916+ReyhanAnf@users.noreply.github.com>
2023-12-17 20:28:45 +0700

    Update app.py

--- coba/app.py
+++ coba/app.py
@@ -3,6 +3,11 @@ import streamlit as st
 #import plotly.express as px
 #from PIL import Image
 
+iris = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+
 st.set_page_config(page_title='Survey Results')
 st.header('Survey Results 20221')
 st.subheader('was the tutorial helpful?')
+
+for i in iris.sepal_length:
+  st.subheader(i)
